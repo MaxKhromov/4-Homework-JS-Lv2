b9f7bc67eac575210e38b54b162ba46f883454cc
MaksimKhromov <sloopemail@gmail.com>
2019-03-21 10:52:12 +0400

    done

---
+++ scripts/textFilter.js
@@ -0,0 +1,6 @@
+﻿const nativeText = document.querySelector('#nativeText').innerHTML;
+document.querySelector('#filteredText').innerHTML = makeQuotesDouble(nativeText);;
+
+function makeQuotesDouble(textToFilter) {
+    return textToFilter.replace(/(?<!\w)'/g, '"');
+}
